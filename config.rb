9884f8498615d1c44b18ab30d650a183ba87efbb
Nobuhito Takeuchi <tnobuhito@gmail.com>
2020-07-26 12:10:16 +0900

    ページネーションに対応

--- config.rb
+++ config.rb
@@ -30,9 +30,9 @@ activate :blog do |blog|
   blog.calendar_template = "calendar.html"
 
   # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
 
   blog.layout = "article_layout"
 end
